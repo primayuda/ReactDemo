ae8e7b419685936042147ac74b9a8c4b14bbaaad
Aprita Primayuda <80294439+primayuda@users.noreply.github.com>
2022-09-14 14:57:52 +0000

    Stock data component

--- src/components/StockList.jsx
+++ src/components/StockList.jsx
@@ -6,7 +6,7 @@ import { useWatchList } from '../context/watchListContext'
 
 export const StockList = () => {
   const [ stock, setStock ] = useState()
-  const { watchList } = useWatchList();
+  const { watchList, deleteStock } = useWatchList();
   const navigate = useNavigate();
 
   const changeColor = (data) => {
@@ -88,7 +88,18 @@ export const StockList = () => {
               <td>{stockData.data.h}</td>
               <td>{stockData.data.l}</td>
               <td>{stockData.data.o}</td>
-              <td>{stockData.data.pc}</td>
+              <td>
+                {stockData.data.pc} 
+                <button 
+                  className="btn btn-danger btn-sm mx-3 d-inline-block delete-button"
+                  onClick={(e) => {
+                    e.stopPropagation()
+                    deleteStock(stockData.symbol)
+                  }}
+                >
+                  Remove
+                </button>
+              </td>
             </tr>
           )
         })}
